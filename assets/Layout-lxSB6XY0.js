cbd25c588637f5332cb5970cec6fe0d3b2a9061b
Yusuf Erman ERGÖZ <yusuf.ergoz@wundermanthompson.com>
2025-01-26 03:08:33 +0300

    Updates

--- assets/Layout-lxSB6XY0.js
+++ assets/Layout-lxSB6XY0.js
@@ -1,4 +1,4 @@
-import{g as gn,R as Pe,j as dt}from"./index-O8RrgrHY.js";/*!
+import{g as gn,R as Pe,j as dt}from"./index-DXT3FRu6.js";/*!
  * Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com
  * License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License)
  * Copyright 2024 Fonticons, Inc.
