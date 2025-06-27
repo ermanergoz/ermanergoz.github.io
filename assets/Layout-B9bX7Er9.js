13b2f3dcea2a6acbdd0590c7d15ba639f61dc4c9
Erman ERGÖZ <ermanergoz@Ermans-MacBook-Air.local>
2025-06-27 23:56:51 +0300

    Updates

--- assets/Layout-B9bX7Er9.js
+++ assets/Layout-B9bX7Er9.js
@@ -1,4 +1,4 @@
-import{g as gn,R as Pe,j as dt}from"./index-BtvDa6ML.js";/*!
+import{g as gn,R as Pe,j as dt}from"./index-obO-WczD.js";/*!
  * Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com
  * License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License)
  * Copyright 2024 Fonticons, Inc.
