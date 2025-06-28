7b7be2d7ea37da8c0b4c81fb570404e610c7c2e5
Erman ERGÖZ <ermanergoz@Ermans-MacBook-Air.local>
2025-06-28 17:39:04 +0300

    Updates

--- assets/Layout-CzW3LriQ.js
+++ assets/Layout-CzW3LriQ.js
@@ -1,4 +1,4 @@
-import{g as gn,R as Pe,j as dt}from"./index-BK8tnECg.js";/*!
+import{g as gn,R as Pe,j as dt}from"./index-Dfx4Q0fc.js";/*!
  * Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com
  * License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License)
  * Copyright 2024 Fonticons, Inc.
